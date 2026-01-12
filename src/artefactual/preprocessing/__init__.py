3466778bc5c4dc4e9ab6dc69321cfb6830236fa4
Charles Moslonka <charles.moslonka@artefact.com>
2026-01-12 19:23:37 +0100

    chore: spelling mistake in init

--- src/artefactual/preprocessing/__init__.py
+++ src/artefactual/preprocessing/__init__.py
@@ -1,10 +1,9 @@
-from aretefactual.preprocessing.parser import parse_model_outputs
-
 from artefactual.preprocessing.openai_parser import (
     is_openai_responses_api,
     process_openai_chat_completion,
     process_openai_responses_api,
 )
+from artefactual.preprocessing.parser import parse_model_outputs
 from artefactual.preprocessing.vllm_parser import process_vllm_logprobs
 
 __all__ = [
