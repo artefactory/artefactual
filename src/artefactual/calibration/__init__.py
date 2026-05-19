e425f06b44f4225377611afbefe5a5c6a0810483
chicham <hicham.randrianarivo@artefact.com>
2026-05-19 17:48:21 +0200

    fix(calibration): defer optional vLLM/ray imports so tests collect without [calibration] extra

--- src/artefactual/calibration/__init__.py
+++ src/artefactual/calibration/__init__.py
@@ -1,22 +1,24 @@
-"""Calibration module for artefactual library.
+"""Calibration module for artefactual library."""
 
-This module exports configuration classes and training functions for calibration.
-"""
-
-from artefactual.calibration.outputs_entropy import GenerationConfig
-from artefactual.calibration.rates_answers import RatingConfig
 from artefactual.calibration.train_calibration import train_calibration
-from artefactual.calibration.utils.io import load_tqa_from_json, save_to_json
-from artefactual.calibration.utils.memory import clear_gpu_memory
-from artefactual.calibration.utils.models import get_model_name, init_llm
 
-__all__ = [
-    "GenerationConfig",
-    "RatingConfig",
-    "clear_gpu_memory",
-    "get_model_name",
-    "init_llm",
-    "load_tqa_from_json",
-    "save_to_json",
-    "train_calibration",
-]
+__all__ = ["train_calibration"]
+
+try:
+    from artefactual.calibration.outputs_entropy import GenerationConfig
+    from artefactual.calibration.rates_answers import RatingConfig
+    from artefactual.calibration.utils.io import load_tqa_from_json, save_to_json
+    from artefactual.calibration.utils.memory import clear_gpu_memory
+    from artefactual.calibration.utils.models import get_model_name, init_llm
+except ImportError:
+    pass
+else:
+    __all__ += [
+        "GenerationConfig",
+        "RatingConfig",
+        "clear_gpu_memory",
+        "get_model_name",
+        "init_llm",
+        "load_tqa_from_json",
+        "save_to_json",
+    ]
