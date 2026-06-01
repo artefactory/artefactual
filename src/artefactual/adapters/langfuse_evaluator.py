50bf3efb1e1d9eaf33e3961ddaad4db2a36cff41
Charlotte Le Bihan <charlotte.lebihan@student-cs.fr>
2026-06-01 17:00:55 +0200

    feat: created HallucinationDetector and HallucinationEvaluator classes

---
+++ src/artefactual/adapters/langfuse_evaluator.py
@@ -0,0 +1,21 @@
+from langfuse import Langfuse
+from artefactual.adapters.hallucination_detector import HallucinationDetector
+from artefactual.preprocessing import parse_top_logprobs
+
+class HallucinationEvaluator:
+    def __init__(self, name: str, langfuse_client: Langfuse, detector: HallucinationDetector) -> None:
+        self.name = name
+        self.langfuse = langfuse_client
+        self.detector = detector
+
+    def score_trace(self, trace_id: str) -> float:
+        trace = self.langfuse.api.trace.get(trace_id)
+        parsed_logprobs = parse_top_logprobs(trace.output)
+        value = self.detector.predict_proba(parsed_logprobs)[0]
+        self.langfuse.create_score(
+            trace_id=trace_id,
+            name=self.name,
+            value=value,
+        )
+        return value
+
